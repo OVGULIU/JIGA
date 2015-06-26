a11ba291daf6eee75accb3c2ea52a232772d7b18
Arjun Narayanan <arjun.narayanan91@gmail.com>
2015-06-26 14:02:02 -0700

    added INC and IEN functions to bspline.jl

--- src/bspline.jl
+++ src/bspline.jl
@@ -1,5 +1,9 @@
 module bspline
-export N, knot_parameters
+export N, knot_parameters, global_basis_number, INC, IEN
+
+# The following function evaluates a b-spline function of order 'p' and local index 'i' corresponding
+# to the knot vector Ξ. The function is evaluated at the point ξ. Clearly, the arguments 'i', 'p' and 'ξ'
+# must be compatible with the knot vector.
 
 function N(i::Int64, p::Int64, Ξ::Array, ξ::Float64)
   if (p < 0)
@@ -42,6 +46,12 @@ function N(i::Int64, p::Int64, Ξ::Array, ξ::Float64)
   end
 end
 
+# The following function returns the primary parameters associated with a knot.
+# The function assumes that the first knot is an open knot, and therefore has multiplicity
+# of (p+1). The function returns a tuple of integers [n,p]. Here, p is highest order basis
+# that can be obtained from the given knot vector. 'n' is the corresponding number of basis
+# vectors of order 'p'.
+
 function knot_parameters(Ξ::Array)
   p = 0
   for i in 1:(length(Ξ) - 1)
@@ -51,10 +61,98 @@ function knot_parameters(Ξ::Array)
     end
   end
 
-  n = length(Ξ) - p
+  n = length(Ξ) - 1 - p
   return n,p
 end
 
+# The following function returns the number of basis functions of order 'p' that are associated with
+# the knot vector Ξ.
+
+function knot_parameters(Ξ::Array, p::Int64)
+  n1,p1 = knot_parameters(Ξ)
+  if p > p1
+    print("Order p must be compatible with knot vector Ξ!")
+    n = NaN
+  else
+    n = n1 + p1 - p
+  end
+  return n
+end
+
+# The following function returns the global basis number corresponding to a tensor product NURBS
+# construction from the knot vectors Ξ and H. By the ordering (Ξ,H) it is implied that Ξ corresponds to
+# "direction 1" and H corresponds to "direction 2". The pair (i,j) corresponds to the NURBS co-ordinate
+# of the vertex. For instance, the pair (3,2) corresponds to the vertex of the intersection of ξ₃ and η₂.
+
+function global_basis_number ( Ξ::Array, H::Array, i::Int64, j::Int64, order::Int64)
+  n1 = knot_parameters(Ξ, order)
+  n2 = knot_parameters(H, order)
+  if i > n1 || j > n2 || i < 1 || j < 1
+    print("Invalid NURBS co-ordinate! Co-ordinate is out of bounds")
+    return NaN
+  else
+    A = n1*(j-1)+i
+  end
+  return A
+end
+
+# The following function returns the INC (INurbsCoordinate) array corresponding to the knot vectors
+# Ξ and H for a given order of bspline basis. The INC array is such that, given a global basis function number
+# (refer to the function global_basis_number) and a co-ordinate direction (i.e. 1 or 2), the INC array returns
+# the local basis numbering in the specified direction
+
+function INC ( Ξ::Array, H::Array, order::Int64 )
+  n1 = knot_parameters(Ξ, order)
+  n2 = knot_parameters(H, order)
+  A_max = global_basis_number( Ξ, H, n1, n2, order)
+  INC_array = zeros(2,A_max)
+  for j = 1:n2
+    for i = 1:n1
+      INC_array[1,n1*(j-1)+i] = i
+      INC_array[2,n1*(j-1)+i] = j
+    end
+  end
+  return INC_array
+end
+
+# The following function returns the element number of the element that is located in the parametric
+# domain Ωᵉ = [ξ(i),ξ(i+1)] x [η(j),η(j+1)]
+
+function element_number( Ξ::Array, H::Array, i::Int64, j::Int64, order::Int64 )
+  n1 = knot_parameters(Ξ, order)
+  n2 = knot_parameters(H, order)
+  if i < order + 1 || i > n1 || j < order + 1 || j > n2
+    print("Invalid NURBS co-ordinate for element. Ensure p+1 <= i <= n ")
+    return NaN
+  else
+    e = (j - order - 1)*(n1 - order) + (i - order)
+    return e
+  end
+end
+
+
+function IEN ( Ξ::Array, H::Array, order::Int64 )
+  n1 = knot_parameters(Ξ, order)
+  n2 = knot_parameters(H, order)
+  A,elmt = 1,0
+  IEN_array = zeros(2,9)
+  for j = 1:n2
+    for i = 1:n1
+      if i >= order+1 && j >= order+1
+        elmt = elmt+1
+        for jloc = 0:order
+          for iloc = 0:order
+            B = A - jloc*n1 - iloc
+            b = jloc*(order+1) + iloc + 1
+            IEN_array[b,e] = B
+          end
+        end
+      end
+      A = A+1
+    end
+  end
+  return IEN_array
+end
 
 # Code beyond this point is still under testing and development
 # Shape function derivative
--- src/nurbs.jl
+++ src/nurbs.jl
@@ -3,20 +3,29 @@ using Color
 include("bspline.jl")
 using bspline
 x = linspace(0,1,1000)
+y = linspace(0,1,1000)
+
 Ξ = Float64[0.,0.,0.,0.5,1.,1.,1.]
 H = Float64[0.,0.,0.,1.,1.,1.]
-n1,p1 = bspline.knot_parameters(Ξ)
-n2,p2 = bspline.knot_parameters(H)
-N1(i,p) = Float64[ N(i,p,Ξ,x1)  for x1 in x ]
-N2(i,p) = Float64[ N(i,p,H,x1)  for x1 in x ]
 
+order = 2
+n1 = bspline.knot_parameters(Ξ,order)
+n2 = bspline.knot_parameters(H,order)
+
+N1(i,p) = Float64[ bspline.N(i,p,Ξ,x1)  for x1 in x ]
+M1(j,p) = Float64[ bspline.N(j,p,H,x1)  for x1 in x ]
+
+bspline.global_basis_number(Ξ,H,1,3,order)
+
+INC_array = bspline.INC(Ξ, H, order)
+IEN_array = bspline.IEN(Ξ, H, order)
 
 colors = distinguishable_colors(30)
 
 plot(#[layer(x = x, y = N(i,1), Geom.line, Theme(default_color = colors[i])) for i in 1:8]...,
-     [layer(x = x, y = N1(j,order), Geom.line, Theme(default_color = colors[j+2])) for j in 1:n1-p1+1]...,
+     [layer(x = x, y = N1(j,2), Geom.line, Theme(default_color = colors[j+2])) for j in 1:n1]...,
      #[layer(x = x, y = N(k,3), Geom.line, Theme(default_color = colors[k+13])) for k in 1:5]...
      )
 
-plot([layer(x = x, y = N2(j,order), Geom.line, Theme(default_color = colors[j+5])) for j in 1:n2-p2+1]...)
+plot(layer(x = x, y = N1(3,1), Geom.line, Theme(default_color = colors[5])))
 
